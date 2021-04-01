f043ab8216fd8e028ad4ec3151a8899ba7bd133a
Matthew <mckaneda@comcast.net>
2021-04-01 13:57:11 -0700

    base app for master

--- src/App.js
+++ src/App.js
@@ -1,25 +1,70 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import { Grid } from "@material-ui/core";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import Dashboard from "./Dashboard"
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      token: null,
+    };
+  }
+
+  componentDidMount() {
+    document.title = "Wildfire Lightning";
+    const token = localStorage.getItem("User-Access-Token");
+    this.setState({
+      token: token,
+    })
+  }
+
+  render() {
+    return (
+      <BrowserRouter>
+        <div className="App-header">
+          {/* <Grid container spacing={8}> */}
+            <Route
+              exact
+              path="/dashboard"
+              component={Dashboard}
+              // render={() => Dashboard}
+            />
+          {/* </Grid> */}
+        </div>
+      </BrowserRouter>
+    )
+  }
+
 }
 
 export default App;
+
+// import logo from './logo.svg';
+// import './App.css';
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
+
+// export default App;
---
+++ src/Dashboard.jsx
@@ -0,0 +1,46 @@
+import React, { Component} from "react";
+import {AppBar, Toolbar, Button, Typography, Grid} from "@material-ui/core";
+import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
+// import GoogleMapReact from "google-map-react";
+import "./App.css";
+
+
+class Dashboard extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {}
+  }
+
+  render() {
+    return (
+      <Grid container spacing={8}>
+        <Grid item xs={12}>
+          <AppBar className="general-top-bar" position="absolute">
+            <Toolbar className="topbar-toolbar">
+              <Typography variant="h5" color="inherit">
+                SJSU
+              </Typography>
+              <Typography variant="h5" color="inherit">
+                Wildfire Lightning
+              </Typography>
+              <Typography variant="h5" color="inherit">
+                Group 29
+              </Typography>
+            </Toolbar>
+          </AppBar>
+        </Grid>
+        {/* <div className="topbar-buffer" /> */}
+        <Grid item sm={3}>
+        {/* This is where map is rendered */}
+        </Grid>
+        <Grid item sm={9}>
+          <div className="map-container">
+
+          </div>
+        </Grid>
+      </Grid>
+    )
+  }
+}
+
+export default Dashboard;
