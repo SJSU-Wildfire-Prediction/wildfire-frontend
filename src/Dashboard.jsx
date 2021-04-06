42cb4937b63e402d5dc0a30107e512220f5b1d7b
Matthew <mckaneda@comcast.net>
2021-04-06 02:51:23 -0700

    sorta working

--- src/Dashboard.jsx
+++ src/Dashboard.jsx
@@ -1,6 +1,7 @@
 import React, { Component} from "react";
 import {AppBar, Toolbar, Button, Typography, Grid} from "@material-ui/core";
 import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
+import 'leaflet/dist/leaflet.css';
 // import GoogleMapReact from "google-map-react";
 import "./App.css";
 
@@ -35,6 +36,17 @@ class Dashboard extends Component {
         <Grid item sm={9}>
           <div className="map-container">
             {/* This is where map is rendered */}
+            <MapContainer center={[51.505, -0.09]} zoom={13} scrollWheelZoom={false}>
+              <TileLayer
+                attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+                url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+              />
+              <Marker position={[51.505, -0.09]}>
+                <Popup>
+                  A pretty CSS3 popup. <br /> Easily customizable.
+                </Popup>
+              </Marker>
+            </MapContainer>
           </div>
         </Grid>
       </Grid>
