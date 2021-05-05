49163d84261c3193f065f87f4ab4328653d3b038
daniellelshen <daniellelshen@gmail.com>
2021-05-04 20:01:57 -0700

    My very first git

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { BrowserRouter, Route, Switch } from "react-router-dom";
 import { Grid } from "@material-ui/core";
 
 import Dashboard from "./Dashboard"
+import LightningHistory from "./LightningHistory"
 
 require('dotenv').config();
 
---
+++ src/LightningHistory.jsx
@@ -0,0 +1,25 @@
+import React, { Component } from "react";
+import {TableContainer, TableHead, TableRow, TableCell, TableBody, Paper } from "@material-ui/core";
+import axios from "axios";
+import "./App.css";
+
+const API_USERNAME = process.env.REACT_APP_METEOMATICS_API_USERNAME
+const API_PASSWORD = process.env.REACT_APP_METEOMATICS_API_PASSWORD
+
+class LightningHistory extends Component {
+    constructor(props) {
+      super(props);
+      this.state = {
+  
+      }
+    }
+  
+    render() {
+      
+      return (
+        <div></div>
+      )
+    }
+  }
+  
+  export default LightningHistory;
