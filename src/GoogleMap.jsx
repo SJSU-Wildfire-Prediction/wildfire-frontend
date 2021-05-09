64d8ccc818de0aa9933b66a230bdae587aa0c10e
Matthew <mckaneda@comcast.net>
2021-05-09 06:56:17 -0700

    sample map markers

--- src/GoogleMap.jsx
+++ src/GoogleMap.jsx
@@ -1,6 +1,9 @@
 import React, { Component } from "react";
 import GoogleMapReact from "google-map-react";
 
+import Marker from './Marker.tsx';
+import Marker_red from './Marker_red.tsx';
+
 const AnyReactComponent = ({ text }) => <div>{text}</div>;
 
 const API_KEY = process.env.REACT_APP_GOOGLE_API_KEY
@@ -24,11 +27,22 @@ class GoogleMap extends Component {
           defaultCenter={this.props.center}
           defaultZoom={this.props.zoom}
         >
-          <AnyReactComponent
+          {/* <AnyReactComponent
             lat={59.955413}
             lng={30.337844}
             text="test marker"
+          /> */}
+          <Marker
+            lat={37.3344476}
+            lng={-121.8826716}
+            name="Sample Lightning"
           />
+          <Marker_red
+            lat={37.3337}
+            lng={-121.9907}
+            name="Sample Lightning Wildfire"
+          />
+
         </GoogleMapReact>
       </div>
     );
